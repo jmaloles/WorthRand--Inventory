375b78d0be79fd894d14942674259076140603cf
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-08-29 09:27:44 +0800

    Added links

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,32 +1,12 @@
 <div id="sidebar-toggle" class="">
-    <nav class="col-lg-2 col-md-3 col-sm-3 col-xs-12 sidebar" {{--style="background-color: #565656;"--}}>
-        <ul class="nav nav-pills nav-stacked col-lg-12 col-md-12 col-sm-12 col-xs-12 nav-sidebar" role="tablist" style="font-size: 14px;">
-            <li class="nav-item"><a class="white" class="nav-link" href=""><span class="glyphicon glyphicon-dashboard"></span>&nbsp;&nbsp;&nbsp; Dashboard</a></li>
-            <li class="nav-item" role="tab">
-
-                <a class="white" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapsUser" aria-expanded="false" aria-controls="collapsUser">
-                    <span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;&nbsp; User
-                </a>
-
-                <div id="collapsUser" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-                    <ul class="nav nav-pills nav-stacked">
-                        <li class="nav-item"><a class="nav-link nav-sub-item white" href="" style="font-size: 12px;"><span class="glyphicon glyphicon-plus"></span>&nbsp;&nbsp;&nbsp; Add User</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item" role="tab">
-
-                <a class="white" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                    <span class="glyphicon glyphicon-asterisk"></span>&nbsp;&nbsp;&nbsp; Item
-                </a>
-
-                <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-                    <ul class="nav nav-pills nav-stacked">
-                        <li class="nav-item"><a class="nav-link nav-sub-item white" href="" style="font-size: 12px;"><span class="glyphicon glyphicon-plus"></span>&nbsp;&nbsp;&nbsp; Add Item</a></li>
-                    </ul>
-                </div>
-
-            </li>
+    <nav class="col-lg-2 col-md-3 col-sm-3 col-xs-12 sidebar">
+        <ul class="nav nav-pills nav-stacked col-lg-12 col-md-12 col-sm-12 col-xs-12 nav-sidebar" role="tablist" style="font-size: 14px;" aria-multiselectable="true">
+            <li class="nav-item active"><a class="white" class="nav-link"><span class="glyphicon glyphicon-dashboard"></span>&nbsp;&nbsp;&nbsp; Dashboard</a></li>
+            <li class="nav-item" role="tab"><a class="white" href="#"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;&nbsp; User</a></li>
+            <li class="nav-item" role="tab"><a class="white" href="#"><i class="fa fa-users"></i>&nbsp;&nbsp;&nbsp; Sales</a></li>
+            <li class="nav-item" role="tab"><a class="white" href="#"><i class="fa fa-cog"></i>&nbsp;&nbsp;&nbsp; Projects</a>
+            <li class="nav-item" role="tab"><a class="white" href="#"><i class="fa fa-cogs"></i>&nbsp;&nbsp;&nbsp; Aftermarkets</a>
+            <li class="nav-item" role="tab"><a class="white" href="#"><i class="fa fa-file-text-o"></i>&nbsp;&nbsp;&nbsp; Seal</a></li>
         </ul>
     </nav>
 </div>
