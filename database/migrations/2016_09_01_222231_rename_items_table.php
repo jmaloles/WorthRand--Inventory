e0d84474a973159246cd580ec8e2fb6201b89deb
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-02 08:26:26 +0800

    Added rename items table migration

--- database/migrations/2016_09_01_222231_rename_items_table.php
+++ database/migrations/2016_09_01_222231_rename_items_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class RenameItemsToCategoriesTable extends Migration
+class RenameItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class RenameItemsToCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::rename('items','categories');
+        Schema::rename('items', 'after_markets');
     }
 
     /**
@@ -22,6 +22,6 @@ class RenameItemsToCategoriesTable extends Migration
      */
     public function down()
     {
-
+        Schema::rename('after_markets', 'items');
     }
 }
