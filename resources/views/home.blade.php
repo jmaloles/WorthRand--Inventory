344a27462cb78f79a59b021483dc1c80f2992e27
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-07 08:18:19 +0800

    Changed container-fluid to container

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,37 +1,20 @@
 @extends('layouts.app')
 
-@section('header')
-    @include('layouts.header')
-@stop
-
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="container">
+        <div class="col-lg-12">
             <div class="row">
                 @include('layouts.sidebar')
-                <div class="col-lg-10 col-md-9 col-sm-9 col-xs-12 col-lg-offset-2 col-sm-offset-3 main">
-                    <br><br>
-                    <div class="page-title">
-                        <div class="row">
-                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <div class="row">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading" style="
-                                            background-color: white; border-color: white;
-                                            box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
-                                            border-radius: 0px 0px 0px 0px; font-size: 22px;
-                                            ">
-                                            DASHBOARD
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                <div class="col-lg-9 col-md-8">
+                    <div class="row">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" style="
+                            border-radius: 0px 0px 0px 0px; font-size: 18px;
+                            ">DASHBOARD</div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
