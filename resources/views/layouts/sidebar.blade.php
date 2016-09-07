75d8f9ac77b22ed6aae5d97877cb18de1c251666
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-07 08:20:38 +0800

    Removed sticky sidebar and switched to div list

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,3 +1,4 @@
+{{--
 <div id="sidebar-toggle" class="">
     <nav class="col-lg-2 col-md-3 col-sm-3 col-xs-12 sidebar">
         <ul class="nav nav-pills nav-stacked col-lg-12 col-md-12 col-sm-12 col-xs-12 nav-sidebar" role="tablist" style="font-size: 14px;" aria-multiselectable="true">
@@ -9,4 +10,14 @@
             <li class="nav-item" role="tab"><a class="white" href="#"><i class="fa fa-file-text-o"></i>&nbsp;&nbsp;&nbsp; Seal</a></li>
         </ul>
     </nav>
+</div>--}}
+<div class="col-lg-3">
+    <div class="list-group">
+        <a href="{{ route('super_admin_dashboard') }}" class="white list-group-item {{ Request::route()->getName() == 'super_admin_dashboard' ? 'active' : '' }}" class="nav-link"><span class="glyphicon glyphicon-dashboard"></span>&nbsp; Dashboard</a>
+        <a class="white list-group-item {{ Request::route()->getName() == 'super_admin_user_index' ? 'active' : '' }}" href="{{ route('super_admin_user_index') }}"><span class="glyphicon glyphicon-user"></span>&nbsp; User</a>
+        <a class="white list-group-item" href="#"><i class="fa fa-users"></i>&nbsp; Sales Engineer</a>
+        <a class="white list-group-item" href="#"><i class="fa fa-cog"></i>&nbsp; Projects</a>
+        <a class="white list-group-item" href="#"><i class="fa fa-cogs"></i>&nbsp; Aftermarkets</a>
+        <a class="white list-group-item" href="#"><i class="fa fa-file-text-o"></i>&nbsp; Seal</a>
+    </div>
 </div>
