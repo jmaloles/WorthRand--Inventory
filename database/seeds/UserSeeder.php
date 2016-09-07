6528c6e5ac34b786d3c256255a2d3d2efff7a6e7
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-07 08:15:06 +0800

    Added user roles

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -20,7 +20,39 @@ class UserSeeder extends Seeder
                     'role'       => env('ROLE', 'super_admin'),
                     'created_at' => date('Y-m-d h:i:s'),
                     'updated_at' => date('Y-m-d h:i:s')
-                )
+                ),
+                array(
+                    'name'       => env('NAME', 'Test Admin'),
+                    'email'      => env('EMAIL', 'test_admin@yahoo.com.com'),
+                    'password'   => bcrypt(env('PASS', 'admin123')),
+                    'role'       => env('ROLE', 'admin'),
+                    'created_at' => date('Y-m-d h:i:s'),
+                    'updated_at' => date('Y-m-d h:i:s')
+                ),
+                array(
+                    'name'       => env('NAME', 'Tester Assistant'),
+                    'email'      => env('EMAIL', 'tester_assistant@yahoo.com'),
+                    'password'   => bcrypt(env('PASS', 'admin123')),
+                    'role'       => env('ROLE', 'assistant'),
+                    'created_at' => date('Y-m-d h:i:s'),
+                    'updated_at' => date('Y-m-d h:i:s')
+                ),
+                array(
+                    'name'       => env('NAME', 'Tester Collection'),
+                    'email'      => env('EMAIL', 'tester_collection@yahoo.com'),
+                    'password'   => bcrypt(env('PASS', 'admin123')),
+                    'role'       => env('ROLE', 'collection'),
+                    'created_at' => date('Y-m-d h:i:s'),
+                    'updated_at' => date('Y-m-d h:i:s')
+                ),
+                array(
+                    'name'       => env('NAME', 'Tester Sales Engineer'),
+                    'email'      => env('EMAIL', 'tester_sales_e@yahoo.com'),
+                    'password'   => bcrypt(env('PASS', 'admin123')),
+                    'role'       => env('ROLE', 'user'),
+                    'created_at' => date('Y-m-d h:i:s'),
+                    'updated_at' => date('Y-m-d h:i:s')
+                ),
             )
         );
     }
