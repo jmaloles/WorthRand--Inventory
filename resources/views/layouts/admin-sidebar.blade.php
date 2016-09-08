f416bb947c88309bad845f96dad196023fb53f19
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-08 16:38:10 +0800

    Added item page link

--- resources/views/layouts/admin-sidebar.blade.php
+++ resources/views/layouts/admin-sidebar.blade.php
@@ -4,7 +4,7 @@
         <a class="white list-group-item {{ Request::route()->getName() == 'admin_user_index' ? 'active' : '' }}" href="{{ route('admin_user_index') }}"><span class="glyphicon glyphicon-user"></span>&nbsp; Users</a>
         <a class="white list-group-item {{ Request::route()->getName() == 'admin_sales_engineer_index' ? 'active' : '' }}" href="{{ route('admin_sales_engineer_index') }}"><i class="fa fa-users"></i>&nbsp; Sales Engineers</a>
         <a class="white list-group-item" href="#"><i class="fa fa-star" aria-hidden="true"></i>&nbsp; Customers</a>
-        <a class="white list-group-item" href="{{ route('items') }}"><i class="fa fa-diamond" aria-hidden="true"></i>&nbsp; Items</a>
+        <a class="white list-group-item {{ Request::route()->getName() == 'items' ? 'active' : '' }}" href="{{ route('items') }}"><i class="fa fa-diamond" aria-hidden="true"></i>&nbsp; Items</a>
         <a class="white list-group-item" href="#"><i class="fa fa-cog"></i>&nbsp; Projects</a>
         <a class="white list-group-item" href="#"><i class="fa fa-cogs"></i>&nbsp; Aftermarkets</a>
         <a class="white list-group-item" href="#"><i class="fa fa-file-text-o"></i>&nbsp; Seal</a>
