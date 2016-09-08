aa89b55d9b907b47a5a0c1231c7bda333b97aa10
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-08 12:11:20 +0800

    Added sales engineers list

---
+++ resources/views/sales_engineer/admin/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('header')
+    @include('layouts.header')
+@stop
+
+@section('content')
+    <div class="container">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="row">
+                @include('layouts.admin-sidebar')
+                <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            SALES ENGINEERS
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="col-lg-12">
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>E-mail</th>
+                                        <th class="text-right">Actions</th>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($users as $user)
+                                            <tr>
+                                                <td>{{ $user->id }}</td>
+                                                <td>{{ $user->name }}</td>
+                                                <td>{{ $user->email }}</td>
+                                                <td class="text-right">
+                                                    <a href="#" class="btn btn-sm btn-danger">Deactivate</a>
+                                                    <a href="#" class="btn btn-sm btn-primary">View Profile</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
