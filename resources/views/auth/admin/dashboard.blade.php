138db00b045144a92edae61caf211ee2a43dd782
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-08 08:47:12 +0800

    Added lavachart piechart for test run

--- resources/views/auth/admin/dashboard.blade.php
+++ resources/views/auth/admin/dashboard.blade.php
@@ -6,16 +6,19 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            @include('layouts.admin-sidebar')
-            <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading" style="
-                    background-color: white; border-color: white;
-                    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
-                    border-radius: 0px 0px 0px 0px; font-size: 22px;
-                    ">
-                        DASHBOARD
+        <div class="col-lg-12">
+            <div class="row">
+                @include('layouts.admin-sidebar')
+                <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            DASHBOARD
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div id="chart-div"></div>
+                        {!! $lava->render('PieChart', 'USERS', 'chart-div') !!}
                     </div>
                 </div>
             </div>
