0d6be24e2f0de6d624f207f2c5326cafeca0916a
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-08 08:38:36 +0800

    Added user index on super_admin side

--- resources/views/user/super_admin/index.blade.php
+++ resources/views/user/super_admin/index.blade.php
@@ -11,7 +11,7 @@
                 @include('layouts.sidebar')
                 <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
                     <div class="panel panel-default">
-                        <div class="panel-heading"">
+                        <div class="panel-heading">
                             USERS
                         </div>
                     </div>
