61a84bbaf629197ce055d0d6138dc48b82cc687f
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-08 08:51:05 +0800

    Added link for user index

--- resources/views/layouts/admin-sidebar.blade.php
+++ resources/views/layouts/admin-sidebar.blade.php
@@ -1,8 +1,9 @@
 <div class="col-lg-3">
     <div class="list-group">
         <a href="{{ route('admin_dashboard') }}" class="white list-group-item {{ Request::route()->getName() == 'admin_dashboard' ? 'active' : '' }}" class="nav-link"><span class="glyphicon glyphicon-dashboard"></span>&nbsp; Dashboard</a>
-        <a class="white list-group-item {{ Request::route()->getName() == 'admin_user_index' ? 'active' : '' }}" href="{{ route('admin_user_index') }}"><span class="glyphicon glyphicon-user"></span>&nbsp; User</a>
-        <a class="white list-group-item" href="#"><i class="fa fa-users"></i>&nbsp; Sales</a>
+        <a class="white list-group-item {{ Request::route()->getName() == 'admin_user_index' ? 'active' : '' }}" href="{{ route('admin_user_index') }}"><span class="glyphicon glyphicon-user"></span>&nbsp; Users</a>
+        <a class="white list-group-item" href="#"><i class="fa fa-users"></i>&nbsp; Sales Engineers</a>
+        <a class="white list-group-item" href="#"><i class="fa fa-star" aria-hidden="true"></i>&nbsp; Customers</a>
         <a class="white list-group-item" href="#"><i class="fa fa-cog"></i>&nbsp; Projects</a>
         <a class="white list-group-item" href="#"><i class="fa fa-cogs"></i>&nbsp; Aftermarkets</a>
         <a class="white list-group-item" href="#"><i class="fa fa-file-text-o"></i>&nbsp; Seal</a>
