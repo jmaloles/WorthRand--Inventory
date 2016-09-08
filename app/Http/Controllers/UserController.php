0e6b7744315ad1bb4d5e5741ce2c69ea85896a62
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-08 08:41:51 +0800

    Added Lavachart graph on admin dashboard for functionality testing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,18 +5,67 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\User;
+use Khill\Lavacharts\Lavacharts;
+use App\Http\Requests\CreateUserRequest;
 
 class UserController extends Controller
 {
     //
     public function superAdminDashboard()
     {
-        return view('home');
+        $users = User::all();
+        // Test if date was submitted
+        /*if($request->date) {
+            $users = $users->whereRaw("date(txt_sms_activities.created_at) = '" . $request->date . "'");
+        }*/
+
+        // create datatable
+        $lava = new Lavacharts();
+
+        $reasons = $lava->DataTable();
+        $reasons->addStringColumn('Users')
+            ->addNumberColumn('Percent');
+        foreach($users as $user) {
+            $reasons->addRow(array($user->name, $user->id));
+        }
+
+        $barchart = $lava->PieChart('USERS', $reasons, [
+            'title' => 'Count per User',
+            'is3D' => true,
+            'height' => 400,
+            'width' => 600
+        ]);
+
+        return view('home')->with('lava', $lava);
     }
 
     public function adminDashboard()
     {
-        return view('super_admin.dashboard');
+        $users = User::all();
+        // Test if date was submitted
+        /*if($request->date) {
+            $users = $users->whereRaw("date(txt_sms_activities.created_at) = '" . $request->date . "'");
+        }*/
+
+        // create datatable
+        $lava = new Lavacharts();
+
+        $reasons = $lava->DataTable();
+        $reasons->addStringColumn('Users')
+            ->addNumberColumn('Percent');
+        foreach($users as $user) {
+            $reasons->addRow(array($user->name, $user->id));
+        }
+
+        $piechart = $lava->PieChart('USERS', $reasons, [
+            'title' => 'Project Sales',
+            'is3D' => true,
+            'height' => 400,
+            'width' => 600
+        ]);
+
+        return view('auth.admin.dashboard', compact('lava'));
     }
 
     public function collectionDashboard()
@@ -36,6 +85,23 @@ class UserController extends Controller
 
     public function superAdminUserIndex()
     {
-        return view('user.admins.index');
+        return view('user.super_admin.index');
+    }
+
+    public function adminUserIndex()
+    {
+        return view('user.admin.index');
+    }
+
+    public function adminCreateUser()
+    {
+        return view('user.admin.create');
+    }
+
+    public function adminPostUser(CreateUserRequest $createUserRequest)
+    {
+        $create_user = User::createUser($createUserRequest);
+
+        return $create_user;
     }
 }
