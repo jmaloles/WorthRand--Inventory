c730d6b8853b4784ae51cc191e51f0ae67f15ea6
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-11 09:11:56 +0800

    Added another PieChart for test

--- resources/views/auth/admin/dashboard.blade.php
+++ resources/views/auth/admin/dashboard.blade.php
@@ -10,25 +10,23 @@
             <div class="row">
                 @include('layouts.admin-sidebar')
                 <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            DASHBOARD
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div id="chart-div"></div>
-                            {!! $lava->render('PieChart', 'USERS', 'chart-div') !!}
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                DASHBOARD
+                            </div>
                         </div>
 
-                        
-                    </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div id="chart"></div>
+                                {!! $lava->render('PieChart', 'USERS', 'chart') !!}
+                            </div>
 
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div id="group-chart-div"></div>
-                            {!! $group_chart->render('PieChart', 'GROUPS', 'group-chart-div') !!}
+                            <div class="col-lg-6">
+                                <div id="group-chart-div"></div>
+                                {!! $group_chart->render('PieChart', 'GROUPS', 'group-chart-div') !!}
+                            </div>
                         </div>
                     </div>
                 </div>
