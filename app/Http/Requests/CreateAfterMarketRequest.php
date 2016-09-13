8b2c896c963240f561213df9c79416976fea66c4
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-13 21:13:39 +0800

    Added create after market request

---
+++ app/Http/Requests/CreateAfterMarketRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class CreateAfterMarketRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'model' => 'required',
+            'part_number' => 'required',
+            'reference_number' => 'required',
+            'material_number' => 'required',
+            'serial_number' => 'required',
+            'tag_number' => 'required',
+            'drawing_number' => 'required|unique:after_markets,drawing_number'
+        ];
+    }
+}
