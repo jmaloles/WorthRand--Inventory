54f405d30e0dc65f6bb8f35a5cf0472cabfeb8da
Jacob Maloles <cobmaloles@gmail.com>
2016-09-13 09:35:59 +0800

    add customer module

---
+++ database/migrations/2016_09_08_092508_create_branches_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBranchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('customer_id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('city');
+            $table->string('postal_code');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop(branches);
+    }
+}
--- resources/views/layouts/admin-sidebar.blade.php
+++ resources/views/layouts/admin-sidebar.blade.php
@@ -3,7 +3,7 @@
         <a href="{{ route('admin_dashboard') }}" class="white list-group-item {{ Request::route()->getName() == 'admin_dashboard' ? 'active' : '' }}" class="nav-link"><span class="glyphicon glyphicon-dashboard"></span>&nbsp; Dashboard</a>
         <a class="white list-group-item {{ Request::route()->getName() == 'admin_user_index' ? 'active' : '' }}" href="{{ route('admin_user_index') }}"><span class="glyphicon glyphicon-user"></span>&nbsp; Users</a>
         <a class="white list-group-item" href="#"><i class="fa fa-users"></i>&nbsp; Sales Engineers</a>
-        <a class="white list-group-item" href="#"><i class="fa fa-star" aria-hidden="true"></i>&nbsp; Customers</a>
+        <a class="white list-group-item {{ Request::route()->getName() == 'admin_customer_index' ? 'active' : ''}}" href="{{route('admin_customer_index')}}"><i class="fa fa-star" aria-hidden="true"></i>&nbsp; Customers</a>
         <a class="white list-group-item" href="#"><i class="fa fa-cog"></i>&nbsp; Projects</a>
         <a class="white list-group-item" href="#"><i class="fa fa-cogs"></i>&nbsp; Aftermarkets</a>
         <a class="white list-group-item" href="#"><i class="fa fa-file-text-o"></i>&nbsp; Seal</a>
