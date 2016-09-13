b8e79b5e3479fa52855849e29fd2f55d7e630f89
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-13 09:31:03 +0800

    Added role on user seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,6 +17,7 @@ class UserSeeder extends Seeder
                     'name'       => env('NAME', 'Test Owner'),
                     'email'      => env('EMAIL', 'test_owner@yahoo.com'),
                     'password'   => bcrypt(env('PASS', 'admin123')),
+                    'role'       => 'owner',
                     'created_at' => date('Y-m-d h:i:s'),
                     'updated_at' => date('Y-m-d h:i:s')
                 ),
@@ -24,6 +25,7 @@ class UserSeeder extends Seeder
                     'name'       => env('NAME', 'Test Admin'),
                     'email'      => env('EMAIL', 'test_admin@yahoo.com'),
                     'password'   => bcrypt(env('PASS', 'admin123')),
+                    'role'       => 'admin',
                     'created_at' => date('Y-m-d h:i:s'),
                     'updated_at' => date('Y-m-d h:i:s')
                 ),
@@ -31,6 +33,7 @@ class UserSeeder extends Seeder
                     'name'       => env('NAME', 'Tester Assistant'),
                     'email'      => env('EMAIL', 'test_assistant@yahoo.com'),
                     'password'   => bcrypt(env('PASS', 'admin123')),
+                    'role'       => 'assistant',
                     'created_at' => date('Y-m-d h:i:s'),
                     'updated_at' => date('Y-m-d h:i:s')
                 ),
@@ -38,6 +41,7 @@ class UserSeeder extends Seeder
                     'name'       => env('NAME', 'Tester Collection'),
                     'email'      => env('EMAIL', 'test_collection@yahoo.com'),
                     'password'   => bcrypt(env('PASS', 'admin123')),
+                    'role'       => 'collection',
                     'created_at' => date('Y-m-d h:i:s'),
                     'updated_at' => date('Y-m-d h:i:s')
                 ),
@@ -45,6 +49,7 @@ class UserSeeder extends Seeder
                     'name'       => env('NAME', 'Tester Sales Engineer'),
                     'email'      => env('EMAIL', 'test_se@yahoo.com'),
                     'password'   => bcrypt(env('PASS', 'admin123')),
+                    'role'       => 'sales_engineer',
                     'created_at' => date('Y-m-d h:i:s'),
                     'updated_at' => date('Y-m-d h:i:s')
                 ),
