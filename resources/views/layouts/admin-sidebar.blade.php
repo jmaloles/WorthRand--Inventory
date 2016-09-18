e101de383aafce0c057c90f8b2e12ef46e99a081
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-18 08:45:00 +0800

    Added pricing history index on admin sidebar

--- resources/views/layouts/admin-sidebar.blade.php
+++ resources/views/layouts/admin-sidebar.blade.php
@@ -19,5 +19,6 @@
         <li class="nav-item {{ Request::route()->getName() == 'admin_project_index' ? 'active' : '' }}"><a class="nav-link"  href="{{ route('admin_project_index') }}"><i class="fa fa-cog"></i>&nbsp; Projects</a></li>
         <li class="nav-item"><a class="nav-link"  href="#"><i class="fa fa-cogs"></i>&nbsp; Aftermarkets</a></li>
         <li class="nav-item"><a class="nav-link"  href="#"><i class="fa fa-file-text-o"></i>&nbsp; Seal</a></li>
+        <li class="nav-item {{ Request::route()->getName() == 'admin_pricing_history_index' ? 'active' : '' }}"><a class="nav-link"  href="{{ route('admin_pricing_history_index') }}"><span class="glyphicon glyphicon-list-alt"></span>&nbsp; Pricing History</a></li>
     </ul>
 </nav>
