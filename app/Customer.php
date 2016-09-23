975c73238c84c8736d679b3cf7c9dde96b511db7
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-23 11:01:50 +0800

    Removed unnecessary condition, and added trims
    Removed else, and added trim to remove the white spaces.

--- app/Customer.php
+++ app/Customer.php
@@ -14,24 +14,14 @@ class Customer extends Model
     public static function createCustomer($createCustomerRequest)
     {
         $customer = new Customer();
-        $customer->name = $createCustomerRequest->get('name');
-        $customer->address = $createCustomerRequest->get('address');
-        $customer->city = $createCustomerRequest->get('city');
-        $customer->postal_code = $createCustomerRequest->get('postal_code');
-        $customer->operation_customer_account_no = $createCustomerRequest->get('operation_customer_account_no');
+        $customer->name = trim(ucfirst($createCustomerRequest->get('name')));
+        $customer->address = trim(ucwords($createCustomerRequest->get('address'), " "));
+        $customer->city = trim(ucfirst($createCustomerRequest->get('city')));
+        $customer->postal_code = trim($createCustomerRequest->get('postal_code'));
+        $customer->operation_customer_account_no = trim($createCustomerRequest->get('operation_customer_account_no'));
 
         if($customer->save()) {
-            $alert = "success";
-            $icon = "check";
-
-            return redirect()->back()->with('message', 'Customer was successfully created')->with('alert', $alert)
-                ->with('icon', $icon);
-        } else {
-            $alert = "danger";
-            $icon = "times";
-
-            return redirect()->back()->with('message', 'Adding customer was unsuccessful')->with('alert', $alert)
-                ->with('icon', $icon);
+            return redirect()->back()->with('message', 'Customer [' . $customer->name . '] was successfully created');
         }
     }
 }
