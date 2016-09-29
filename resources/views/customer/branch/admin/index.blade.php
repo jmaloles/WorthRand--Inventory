63267a961e00f65690f0c02bcc9058b32dca11ea
Jacob Maloles <cobmaloles@gmail.com>
2016-09-29 11:21:52 +0800

    Add

---
+++ resources/views/customer/branch/admin/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('header')
+    @include('layouts.header')
+@stop
+
+@section('content')
+    <div class="container-fluid">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="row">
+                @include('layouts.admin-sidebar')
+                <div class="col-lg-10 col-md-9 col-sm-9 col-xs-12 col-lg-offset-2 col-sm-offset-3 main">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Branches
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
