2687f9666a5d4216e91414aa2cc53c279fa2227d
Jacob Maloles <cobmaloles@gmail.com>
2016-10-12 13:39:11 +0800

    seal module

--- resources/views/item/project/admin/show.blade.php
+++ resources/views/item/project/admin/show.blade.php
@@ -17,6 +17,7 @@
                                     <li><a href="{{ route('admin_project_show', $project->id) }}"><i class="fa fa-cog"></i>&nbsp;Profile</a></li>
                                     <li><a href="{{ route('admin_project_information', $project->id) }}"><i class="fa fa-pencil"></i>&nbsp;Update Information</a></li>
                                     <li class="nav-item"><a class="nav-link"  href="{{ route('admin_create_aftermarket_on_project', $project->id) }}"><i class="fa fa-plus"></i>&nbsp; Add AfterMarket</a></li>
+                                    <li class="nav-item"><a class="nav-link"  href="{{ route('admin_seal_create', $project->id) }}"><i class="fa fa-plus"></i>&nbsp; Add Seal</a></li>
                                 </ul>
                             </li>
                         </li>
